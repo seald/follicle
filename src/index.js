9807ce44867c86d971aa5f9c6b81e5272ee9cbac
Timothée Rebours <t.rebours@gmail.com>
2018-03-28 17:02:44 +0200

    modify export signature of index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 'use strict'
 
 import { connect } from './lib/connect'
-import Document from './lib/document'
-import EmbeddedDocument from './lib/embedded-document'
 
-export {connect, Document, EmbeddedDocument}
+export default connect
